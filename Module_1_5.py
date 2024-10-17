c8c9a961b520c8cc47d7f45aa82297e36bd4659f
EkaBond <ekabond87@gmail.com>
2024-10-17 20:07:47 +0300

    Add files via upload

---
+++ Module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = tuple([2, 'hi', '5', True])
+print('immutable_var:', immutable_var)
+
+# immutable_var[0] = 9          закоментировала, чтобы не останавливал весь алгоритм.
+# print(immutable_var[0])
+
+mutable_list = [1, '5', 'hi', False]
+mutable_list[0] = True
+mutable_list[-1] = 'Kate'
+print('mutable_list:', mutable_list)
+
